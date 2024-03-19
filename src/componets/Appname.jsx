0f6efbc6549b3489fadd34c7e651e3ebf3e6e17a
abdulrehmankhalid906 <abdulrehmankhalid906@gmail.com>
2024-03-19 20:34:24 +0500

    First Commit

---
+++ src/componets/Appname.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Appname()
+{
+    const name = "Ecommerce Project";
+
+    return(
+        <>
+            <h3>{name}</h3>
+        </>
+    );
+}
+
+export default Appname;
